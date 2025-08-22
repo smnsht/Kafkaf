f24a78aa90bbef06c426b6c169382d60290525f2
Simon Shtock <SimonS@InfraEdge.co.il>
2025-08-22 20:02:48 +0300

    delete old pages

--- CachingExtensions.cs
+++ CachingExtensions.cs
@@ -6,4 +6,7 @@ public static class CachingExtensions
 {
     public static string CacheKey(this ClusterConfigOptions clusterConfig) 
         => $"$CCO:{clusterConfig.Alias}/{clusterConfig.Address}";
+
+    public static string CacheKey(this ClusterConfigOptions clusterConfig, string topicName)
+        => $"$TOPIC:{clusterConfig.Alias}/{clusterConfig.Address}/{topicName}";
 }
