ed7d8922062f02df66ba3901bca4a650424d5f8d
Simon Shtock <SimonS@InfraEdge.co.il>
2025-08-23 16:18:33 +0300

    create smple Spinner and SpinnerButton

--- KafkaUtils.cs
+++ KafkaUtils.cs
@@ -1,10 +1,11 @@
 ﻿using Confluent.Kafka;
+using Confluent.Kafka.Admin;
 using Kafkaf.Config;
 
 namespace Kafkaf;
 
 public static class KafkaUtils
-{    
+{
     public static Task<Metadata> GetMetadata(ClusterConfigOptions clusterConfig, int timeoutSeconds = 5)
     {
         var config = new AdminClientConfig
@@ -19,8 +20,72 @@ public static class KafkaUtils
             using var adminClient = new AdminClientBuilder(config)
                 .Build();
 
-            return adminClient.GetMetadata(timeout);          
-        });        
+            return adminClient.GetMetadata(timeout);
+        });
     }
 
+    public static async Task PurgeMessages(ClusterConfigOptions clusterConfig, IEnumerable<string> topicNames, int timeoutSeconds = 5)
+    {
+        if (topicNames?.Any() == true)
+        {
+            var configEntries = new List<ConfigEntry>
+        {
+            new ConfigEntry()
+            {
+                Name = "retention.ms",
+                Value = "1000"
+            }
+        };
+
+            var configs = topicNames.Aggregate(new Dictionary<ConfigResource, List<ConfigEntry>>(), (acc, topicName) =>
+            {
+                var res = new ConfigResource()
+                {
+                    Name = topicName,
+                    Type = ResourceType.Topic,
+                };
+
+                acc.Add(res, configEntries);
+                return acc;
+            });
+
+            var config = new AdminClientConfig
+            {
+                BootstrapServers = clusterConfig.Address
+            };
+
+            var timeout = TimeSpan.FromSeconds(timeoutSeconds);
+
+            using var adminClient = new AdminClientBuilder(config)
+                    .Build();
+
+            await adminClient.AlterConfigsAsync(configs);
+        }               
+    }
+
+    public static Task DeleteTopics(ClusterConfigOptions clusterConfig, IEnumerable<string> topicNames, int timeoutSeconds = 5)
+    {
+        if (topicNames == null || topicNames.Count() == 0)
+        {
+            return Task.CompletedTask;
+        }
+
+        var config = new AdminClientConfig
+        {
+            BootstrapServers = clusterConfig.Address
+        };
+
+        var timeout = TimeSpan.FromSeconds(timeoutSeconds);
+
+        return Task.Run(() =>
+        {
+            using var adminClient = new AdminClientBuilder(config)
+                .Build();
+
+            return adminClient.DeleteTopicsAsync(topicNames, new DeleteTopicsOptions() 
+            { 
+                OperationTimeout = timeout 
+            });
+        });
+    }
 }
