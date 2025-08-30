0f384eb8018939a5235660eaccf3daffa29967d1
Simon Shtock <SimonS@InfraEdge.co.il>
2025-08-30 21:13:37 +0300

    create broker tabs (pages)

---
+++ Components/Pages/Brokers/Configs.razor.cs
@@ -0,0 +1,12 @@
+﻿using Kafkaf.Components.Partials.Broker;
+using Kafkaf.Components.UI;
+using Microsoft.AspNetCore.Components;
+
+namespace Kafkaf.Components.Pages.Brokers;
+
+[Route(BrokerComponent.ROUTE_CONFIGS)]
+public partial class Configs : ClusterIndexAwarePage
+{
+    [Parameter]
+    public int brokerIdx { get; set; }
+}
