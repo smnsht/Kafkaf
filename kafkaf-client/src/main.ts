9ccf3bf23cd40147c4af4dc617682888e7d375b4
Simon S <SimonS@InfraEdge.co.il>
2025-11-03 18:13:10 +0200

    get rid of top-level await

--- kafkaf-client/src/main.ts
+++ kafkaf-client/src/main.ts
@@ -2,8 +2,5 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
 
-try {
-  await bootstrapApplication(App, appConfig);
-} catch (error) {
-  console.error(error);
-}
+bootstrapApplication(App, appConfig)
+  .catch(error => console.error(error));
