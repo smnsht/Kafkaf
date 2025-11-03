34ac1a961655eaccdf63a943d5506dc604d7451b
Simon S <SimonS@InfraEdge.co.il>
2025-11-03 09:55:43 +0200

    finish consumer-details

--- kafkaf-client/src/app/components/pages/consumer-details/consumer-details.ts
+++ kafkaf-client/src/app/components/pages/consumer-details/consumer-details.ts
@@ -1,26 +1,40 @@
-import { JsonPipe } from '@angular/common';
 import { Component, computed, inject, OnInit, signal } from '@angular/core';
 import { ActivatedRoute, RouterLink } from '@angular/router';
 import { ConsumersStore } from '@app/store/consumers/consumers.service';
+import { PageWrapper } from '@app/components/shared/page-wrapper/page-wrapper';
+import { KafkafTableDirective } from '@app/directives/kafkaf-table/kafkaf-table';
+import { StatsCard, StatsCardItem } from '@app/components/shared/stats-card/stats-card';
+import { ConsumerGroupRow } from '@app/store/consumers/consumer-group-row.model';
+import { Search } from '@app/components/shared/search/search/search';
+
+const defaultCardItems: readonly StatsCardItem[] = [
+  { label: 'State', value: '?' },
+  { label: 'Members', value: '?' },
+  { label: 'Assigned Topics', value: '?' },
+  { label: 'Assigned Partitions', value: '?' },
+  { label: 'Coordinator ID', value: '?' },
+  { label: 'Total lag', value: '?' },
+];
 
 @Component({
   selector: 'app-consumer-details',
-  imports: [RouterLink, JsonPipe],
+  imports: [RouterLink, PageWrapper, KafkafTableDirective, StatsCard, Search],
   templateUrl: './consumer-details.html',
-  styleUrl: './consumer-details.scss'
+  styleUrl: './consumer-details.scss',
 })
 export class ConsumerDetails implements OnInit {
   readonly store = inject(ConsumersStore);
   readonly route = inject(ActivatedRoute);
 
   readonly consumerParam = signal<string | undefined>(undefined);
+  readonly search = signal('');
 
   readonly consumer = computed(() => {
     const groupId = this.consumerParam();
     const consumers = this.store.consumers();
 
     if (groupId && consumers) {
-      return consumers.find(c => c.groupId === groupId);
+      return consumers.find((c) => c.groupId === groupId);
     }
 
     return undefined;
@@ -28,16 +42,76 @@ export class ConsumerDetails implements OnInit {
 
   readonly partitions = computed(() => {
     const consumer = this.consumer();
-    return consumer?.partitions;
+    const search = this.search().toLowerCase();
+
+    return consumer?.partitions.filter((part) => {
+      if (search) {
+        return part.topic.toLowerCase().includes(search);
+      }
+
+      return true;
+    });
+  });
+
+  readonly cardItems = computed<StatsCardItem[]>(() => {
+    const consumer = this.consumer();
+
+    if (consumer) {
+      return this.buildCarItems(consumer);
+    }
+
+    return [...defaultCardItems];
   });
 
   cluster = 0;
 
   ngOnInit(): void {
     this.store.loadConsumers();
-    this.route.paramMap.subscribe(params => {
+    this.route.paramMap.subscribe((params) => {
       this.cluster = Number.parseInt(params.get('cluster')!);
       this.consumerParam.set(params.get('consumer')!);
     });
+  }
+
+  private buildCarItems(consumer: ConsumerGroupRow): StatsCardItem[] {
+    const cardItems = [...defaultCardItems];
+
+    // State
+    cardItems[0] = {
+      ...cardItems[0],
+      value: consumer.state,
+    };
+
+    // Members
+    cardItems[1] = {
+      ...cardItems[1],
+      value: consumer.numberOfMembers,
+    };
+
+    // Assigned Topics
+    cardItems[2] = {
+      ...cardItems[2],
+      value: consumer.numberOfTopics,
+    };
+
+    // Assigned Partitions
+    cardItems[3] = {
+      ...cardItems[3],
+      value: consumer.partitions.length,
+    };
+
+    // Coordinator ID
+    cardItems[4] = {
+      ...cardItems[4],
+      value: consumer.coordinator,
+    };
+
+    // Total lag
+    cardItems[5] = {
+      ...cardItems[5],
+      value: consumer.consumerLag,
+    };
+
+    return cardItems;
   }
 }
